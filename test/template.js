3946a186514bae517cc0ac6bbb2dfa2ddcce1d9a
Manuel Riezebosch <mriezebosch@gmail.com>
2017-03-23 09:29:33 +0100

    template parsing met mustache

--- test/template.js
+++ test/template.js
@@ -1,12 +1,29 @@
 var should = require('should');
 var fs = require('fs');
 var path = require('path');
+var mustache = require('mustache');
 
 describe('template', () => {
     it('should render', () => {
-        var t = path.join(__filename, '../../src/template/reveal.html');
-        var template = fs.readFileSync(t);
+        var data = { 
+            title: 'test'
+        };
+        template(data)
+            .toString().should.match(/<div class="reveal"/m);
+    });
 
-        template.toString().should.match(/<div class="reveal"/m);
+    it ('should list a slide', () => {
+        var data = { 
+            slides: [ { path: '00-intro.md' }],
+            title: 'test'
+        };
+
+        template(data)
+            .toString().should.match(/<section data-markdown="slides\/00-intro.md"/m);
     })
+
+    function template(data) {
+        var f = path.join(__filename, '../../src/template/reveal.html');
+        return mustache.to_html(fs.readFileSync(f).toString(), data);
+    }
 })
