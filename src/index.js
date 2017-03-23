a67845afa28b8901fe74be1ad4214b512e552aa1
Manuel Riezebosch <mriezebosch@gmail.com>
2017-03-23 12:14:55 +0100

    missing dependencies

--- src/index.js
+++ src/index.js
@@ -10,42 +10,12 @@ program
   .version('0.0.11')
 
 program
-  .command('init')
-  .description('Alias for "yo @infosupport/kc"')
-  .action(function(cmd, options) { 
-    spawn('yo', ['@infosupport/kc'], { stdio: 'inherit', });
-  })
-  .on('--help', function() {
-    console.log('  (re)scaffold a presentation');
-  });
-
-program
-  .command('serve')
-  .description('Alias for "gulp serve"')
+  .command('')
+  .description('serve presentation slides directory')
   .action(function(cmd, options) {
     spawn('gulp', ['serve'], { stdio: 'inherit'});
   }).on('--help', function () {
-    console.log('  serve presentation on localhost with live reload')
   });
-
-program
-  .command('print')
-  .description('Print slide deck to PDF')
-  .action(function(cmd, options) {
-    spawn('gulp', ['print'], { stdio: 'inherit'});
-  }).on('--help', function () {
-    console.log('  print slide deck to PDF using phantomjs')
-  });
-  
-program
-  .command('install')
-  .description('Alias for npm install && bower install')
-  .action(function(cmd, options) {
-    spawn('npm', ['install'], { stdio: 'inherit'});
-    spawn('bower', ['install'], { stdio: 'inherit'});
-  }).on('--help', function () {
-    console.log('  install local dependencies from package.json and bower.json')
-  });  
 
 program.parse(process.argv);
 
