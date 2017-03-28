cb57c0202ac26cedeef39129c02d175583f00d9a
Manuel Riezebosch <mriezebosch@gmail.com>
2017-03-28 09:14:18 +0200

    36 total mutants, 36 mutants killed.

---
+++ stryker.conf.js
@@ -0,0 +1,17 @@
+module.exports = function(config){
+  config.set({
+    files: [
+        // Add your files here, this is just an example:
+        { pattern: 'src/**/*.js', mutated: true, included: false},
+        '!src/index.js',
+        'test/**/*.js',
+        { pattern: 'test/test_data/**/*', mutated: false, included: false },
+        { pattern: 'src/template/*', mutated: false, included: false },
+        { pattern: 'src/theme/*', mutated: false, included: false },
+    ],
+    testRunner: 'mocha',
+    testFramework: 'mocha',
+    coverageAnalysis: 'perTest',
+    reporter: ['clear-text', 'html', 'progress'],
+  });
+}
