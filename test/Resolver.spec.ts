cb1e3bf0de6f50a80a821ce1c4dbf277a634080c
Manuel Riezebosch <mriezebosch@gmail.com>
2017-04-12 15:11:24 +0200

    refactor(quality): seperate slide listing from transformation
    introduced Slide and SlideFolder object implementing interface SlideObject
    using waldir to list all slides recursively
    using lodash to group slides from subfolders into SlideFolder object

--- test/Resolver.spec.ts
+++ test/Resolver.spec.ts
@@ -1,7 +1,7 @@
 import 'should';
 import * as path from 'path';
 import { Resolver } from '../src/Resolver';
-import { SlideObject } from '../src/SlideObject';
+import { Slide, SlideFolder } from '../src/SlideObject';
 import { expect } from 'chai';
 
 describe('Resolver', () => {
@@ -11,18 +11,20 @@ describe('Resolver', () => {
         it('should iterate folders and files', async () => {
             const slides = await new Resolver(path.join(__dirname, 'test_data')).slides();
             expect(slides).to.deep.eq([
-                new SlideObject('00-intro.md'),
-                [
-                    new SlideObject(path.join('01-sub', '00-title.md')),
-                    new SlideObject(path.join('01-sub', '01-sub-folder', '01-sub-item.md')),
-                    new SlideObject(path.join('01-sub', '02-item.png'))
-                ]
+                new Slide('00-intro.md'),
+                new SlideFolder('01-sub', [
+                    new Slide(path.join('01-sub', '00-title.md')),
+                    new Slide(path.join('01-sub', '01-sub-folder', '01-sub-item.md')),
+                    new Slide(path.join('01-sub', '02-item.png'))
+                ])
             ]);
         });
 
         it('should not fail on no folder', async () => {
             expect(await new Resolver('asdfasdfasdf').slides()).to.deep.eq([]);
         });
+
+        
     });
 
 
