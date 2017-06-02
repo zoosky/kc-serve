3cef79262fb1d6414557bd9903c6ab6aa280bce6
Manuel Riezebosch <mriezebosch@gmail.com>
2017-06-02 09:24:37 +0200

    resolve-bin for electron-pdf

--- src/Printer.ts
+++ src/Printer.ts
@@ -1,7 +1,17 @@
-const electronpdf = require.resolve('electron-pdf/cli.js');
+const resolveBin = require('resolve-bin');
 const spawn = require('spawn-please');
 export default class {
     async print(url: string, file: string) {
-        return spawn('node', [electronpdf, `${url}?print-pdf`, file]);
+        const electronpdf = await resolve('electron-pdf');
+        await spawn('node', [electronpdf, `${url}?print-pdf`, file]);
     }
+}
+
+export function resolve(name: string) {
+    return new Promise((resolve, reject) => resolveBin(name, (error: Error, bin: string) => {
+        if (error) {
+            reject(error);
+        }
+        resolve(bin);
+    }));
 }
--- test/Printer.spec.ts
+++ test/Printer.spec.ts
@@ -2,6 +2,7 @@ import { expect } from 'chai';
 import * as fs from 'mz/fs';
 import Printer from '../src/Printer';
 import Server from '../src/Server';
+import { resolve } from '../src/Printer';
 
 describe('Printer', function () {
     this.timeout(20000);
@@ -19,6 +20,19 @@ describe('Printer', function () {
         await server.close();
 
         expect(await fs.exists(output)).to.be.true;
+    });
+
+    it('should resolve async', async () => {
+        expect(await(resolve('electron-pdf'))).to.match(/electron-pdf/);
+    });
+
+    it('should throw when not found', async () => {
+        try {
+            await(resolve('electron-non-existing'));
+            expect.fail();
+        } catch (err) {
+            expect(err.message).to.eq('Cannot find module \'electron-non-existing\'');
+        }
     });
 });
 
