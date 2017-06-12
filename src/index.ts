a839403e2c9a8c03b06697ea6216bdf19f21e26b
Manuel Riezebosch <mriezebosch@gmail.com>
2017-06-12 14:01:00 +0200

    get rid of all.ts and import where needed

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,39 @@
-import * as plugins from './plugins/all';
-import * as templates from './template/all';
+import CustomCss from './plugins/CustomCss';
+import Highlight from './plugins/Highlight';
+import Img from './plugins/Img';
+import Reveal from './plugins/Reveal';
+import Slides from './plugins/Slides';
+import Template from './plugins/Template';
+import Theme from './plugins/Theme';
+
+import IndexTemplate from './template/Index';
+import SlidesTemplate from './template/Slides';
+import CustomCssTemplate from './template/CustomCss';
+import * as RevealTemplate from './template/Reveal';
+import ThemeTemplate from './template/Theme';
+
 import Server from './Server';
 
 export default function (cwd: string, title: string): Server {
-    let reveal = new plugins.Reveal();
-    let css = new plugins.CustomCss(cwd);
-    let highlight = new plugins.Highlight();
-    let slides = new plugins.Slides(cwd);
-    let theme = new plugins.Theme();
-    let img = new plugins.Img(cwd);
+    let reveal = new Reveal();
+    let css = new CustomCss(cwd);
+    let highlight = new Highlight();
+    let slides = new Slides(cwd);
+    let theme = new Theme();
+    let img = new Img(cwd);
 
-    let index = new templates.Index(title,
+    let index = new IndexTemplate(title,
         [
-            new templates.Reveal.Css(reveal.path),
-            new templates.Theme(theme.path),
-            new templates.CustomCss(css, css.path),
-            new templates.Reveal.PdfScript(reveal.path)
+            new RevealTemplate.Css(reveal.path),
+            new ThemeTemplate(theme.path),
+            new CustomCssTemplate(css, css.path),
+            new RevealTemplate.PdfScript(reveal.path)
         ],
         [
-            new templates.Slides(slides, slides.path),
-            new templates.Reveal.MainScript(reveal.path)
+            new SlidesTemplate(slides, slides.path),
+            new RevealTemplate.MainScript(reveal.path)
         ]);
-    let template = new plugins.Template(index);
+    let template = new Template(index);
 
     return new Server([reveal, css, highlight, slides, theme, template, img]);
 }
