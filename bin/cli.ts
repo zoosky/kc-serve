3acbe2bc4f21b60fdb49454a48fe700cb5fcdbee
Manuel Riezebosch <mriezebosch@gmail.com>
2017-06-13 14:43:06 +0200

    official reveal.js release

--- bin/cli.ts
+++ bin/cli.ts
@@ -19,8 +19,8 @@ function start(theme: string, highlight: string, port: number) {
         serve({
             cwd: dir,
             title: path.basename(dir),
-            theme:  theme || 'reveal.js/css/theme/beige.css',
-            highlight: highlight || 'highlight.js/styles/vs.css'
+            theme:  theme || 'reveal.js/css/theme/simple.css',
+            highlight: highlight || 'highlight.js/styles/darkula.css'
         }).listen(port || 0)
             .then(url => console.log('Presentation started on %s', url))
             .catch(err => console.error(err.message));
@@ -42,18 +42,19 @@ function usage() {
     console.log('  --port:\t serve from specified port');
     console.log('');
     console.log('  Pick a highlight style from https://highlightjs.org/static/demo/,');
-    console.log('    or use the css file name https://github.com/isagalaev/highlight.js/tree/master/src/styles');
+    console.log('    or use the css file name from https://github.com/isagalaev/highlight.js/tree/master/src/styles');
     console.log('');
-    console.log('  Use a default reveal.js theme: https://github.com/hakimel/reveal.js#theming,');
+    console.log('  Use a default reveal.js theme: black, white, league, beige, sky, night, serif, simple or solarized');
+    console.log('      see: https://github.com/hakimel/reveal.js#theming');
     console.log('    or specify a package containing a custom theme,');
-    console.log('    or override (parts of) a theme with a custom css from the ./css folder');
+    console.log('    or override (parts of) a theme with a custom css in the ./css folder');
     console.log('');
 
     console.log('Tips:');
     console.log('  * Order slides with a number prefix (you may use leading zeros but it is not required)');
-    console.log('  * Group slides in subdirectories to create vertical slides: https://github.com/hakimel/reveal.js/#markup');
+    console.log('  * Group slides in subdirectories to create vertical slides, see: https://github.com/hakimel/reveal.js/#markup');
     console.log('  * Make image slides directly from `png`, jpg`, `gif` and `svg`');
     console.log('  * Inject custom css from the `./css` folder');
-    console.log('  * Put images in an `./img` folder and refer from a markdown slide with `![alt-text](img/your-image.jpg)`');
-    console.log('  * Style images from the custom-css with a selector on the alt-text `img[alt=\'alt-text\']` or filename `img[src=\'img/your-image.jpg\']`');
+    console.log('  * Put images in an `./img` folder and include in a markdown slide with `![alt-text](img/your-image.jpg)`');
+    console.log('  * Style images from custom-css with a selector on the alt-text `img[alt=\'alt-text\']` or filename `img[src=\'img/your-image.jpg\']`');
 }
